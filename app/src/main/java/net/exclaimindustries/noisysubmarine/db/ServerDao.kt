c4b128d0408c0ae4a0e1099355af78b2a6002dac
Nicholas Killewald <captainspam@gmail.com>
2025-05-15 09:52:57 -0700

    Added updateServer to ServerDao.
    Forgot that one.

--- app/src/main/java/net/exclaimindustries/noisysubmarine/db/ServerDao.kt
+++ app/src/main/java/net/exclaimindustries/noisysubmarine/db/ServerDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 /** The DAO for accessing configured servers. */
@@ -16,6 +17,10 @@ interface ServerDao {
     /** Delete a server configuration from the database. */
     @Delete
     fun deleteServer(server: Server)
+
+    /** Update a server configuration in the database. */
+    @Update
+    fun updateServer(server: Server)
 
     /** Get all the configured servers. */
     @Query("SELECT * FROM servers")
